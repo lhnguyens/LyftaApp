b60c00c6815acb226f0df65a8a229bdf5cb3c872
Luan Nguyen <lh.nguyens@gmail.com>
2019-01-17 14:35:00 +0100

    Initial Commit

---
+++ AppTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTest2
+//
+//  Created by Luan Nguyen on 2019-01-17.
+//  Copyright © 2019 Luan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
